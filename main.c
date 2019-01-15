1f687813ca837c1da55553f95247c1a0780abf0a
第一次 <123456456@qq.com>
2019-01-15 11:12:23 +0800

    添加存钱功能

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     printf("进入ATM管理系统\n");
 
-    while(1){
-        int qian=0;
+    while (1)
+    {
+        int qian = 0;
 
         printf("1---存钱\n");
         printf("2---取钱\n");
@@ -12,24 +14,32 @@ int main(){
 
         printf("请选择相应的功能编号\n");
         int code;
-        scanf("%d",&code);
+        scanf("%d", &code);
 
-        if(code==1){
+        if (code == 1)
+        {
+            printf("请输入存钱金额\n");
+            int qian;
+
+            printf("存钱成功，点击回车继续\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
 
         }
 
-        if(code==2){
-            
+        if (code == 2)
+        {
         }
 
-        if(code==3){
-            
+        if (code == 3)
+        {
         }
 
-        if(code==4){
-            
+        if (code == 4)
+        {
         }
     }
-    
+
     return 0;
 }
