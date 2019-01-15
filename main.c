65993b39c49de190d8d3421bbff44111fc8c5ccb
第一次 <123456456@qq.com>
2019-01-15 11:53:42 +0800

    第二次修改错误

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main()
 {
+    int qian = 0;
     printf("进入ATM管理系统\n");
 
     while (1)
     {
-        int qian = 0;
-
         printf("1---存钱\n");
         printf("2---取钱\n");
         printf("3---余额\n");
@@ -20,36 +19,43 @@ int main()
         {
             printf("请输入存钱金额\n");
             int cun;
-            qian=qian+cun;
-            scanf("%d",&cun);
+            scanf("%d", &cun);
+            qian = qian + cun;
 
             printf("存钱成功，点击回车继续\n");
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
-
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
 
         if (code == 2)
         {
+
             printf("请输入取钱金额\n");
             int qu;
-            qian=qian-qu;
-            scanf("%d",qu);
+            scanf("%d", &qu);
+            if (qu < qian)
+            {
 
-            printf("取钱成功，点击回车继续\n");
-            char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
-            
+                qian = qian - qu;
+                printf("取钱成功，点击回车继续\n");
+            }
+            else
+            {
+                printf("余额不足，取钱失败\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
         }
 
         if (code == 3)
         {
-            printf("您的余额为%d\n",qian);
+            printf("您的余额为:%d\n", qian);
+
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
 
         if (code == 4)
