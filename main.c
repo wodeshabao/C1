cb2122ee303c22c42a2f887520108e64deaf4a10
第一次 <123456456@qq.com>
2019-01-15 10:43:15 +0800

    基本框架

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+    printf("进入ATM管理系统\n");
+
+    while(1){
+        int qian=0;
+
+        printf("1---存钱\n");
+        printf("2---取钱\n");
+        printf("3---余额\n");
+        printf("4---退出\n");
+
+        printf("请选择相应的功能编号\n");
+        int code;
+        scanf("%d",&code);
+
+        if(code==1){
+
+        }
+
+        if(code==2){
+            
+        }
+
+        if(code==3){
+            
+        }
+
+        if(code==4){
+            
+        }
+    }
+    
+    return 0;
+}
